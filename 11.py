c55049fb72d085c22de39f71cc76b21c0adf90b5
allam.vignesh <allam.vignesh@gmail.com>
2021-03-03 23:31:45 +0530

    initial commit

--- 11.py
+++ 11.py
@@ -1,10 +1,13 @@
 def unique(lst):
-    fin = []
-    for i in lst:
-        if i not in fin:
-            fin.append(i)
-    return fin
-
+	un = []
+	qw = []
+	for i in lst:
+		un.append(lst.count(i))
+	for i in range(len(un)):
+		if un[i] == 1:
+			qw.append(lst[i])
+	return qw
+ 
 for i in range(4):
     a = eval(input('Enter list: '))
     print(unique(a))
--- 12.py
+++ 12.py
@@ -1,3 +1,5 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
+fin = [i for i in list(set(a+b)) if (i in a and i in b)]
+print(fin)
